f0de72e2b5f688b41e23866dfa98f82f9fdde567
Joa Ebert <joa.ebert@ltnglobal.com>
2022-01-07 10:47:12 +0100

    don't convert strings

--- try/func.go
+++ try/func.go
@@ -1,16 +1,36 @@
 package try
 
+import (
+	"errors"
+	"fmt"
+)
+
 func Map[T, U any](a Try[T], f func(T) U) Try[U] {
 	return FlatMap(a, func(x T) Try[U] {
 		return Success(f(x))
 	})
 }
 
-func FlatMap[T, U any](a Try[T], f func(T) Try[U]) Try[U] {
+func FlatMap[T, U any](a Try[T], f func(T) Try[U]) (res Try[U]) {
 	if a.Failure() {
 		// TODO: if T == U we can get rid of allocation
 		return Failure[U](a.Err())
 	}
 
-	return f(a.Must())
+	defer func() {
+		if r := recover(); r != nil {
+			switch r := r.(type) {
+			case error:
+				res = Failure[U](r)
+			case string:
+				res = Failure[U](errors.New(r))
+			default:
+				res = Failure[U](fmt.Errorf("%v", r))
+			}
+		}
+	}()
+
+	res = f(a.Must())
+
+	return
 }
--- try/func_test.go
+++ try/func_test.go
@@ -15,21 +15,41 @@ func TestFlatMap(t *testing.T) {
 
 	switch {
 	case a.Failure():
-		t.Error("a.Reject() must be false")
+		t.Error("a.Failure() must be false")
 	case a.Err() != nil:
 		t.Error("a.Err() must be nil")
 	case a.Success() != true:
-		t.Error("a.Resolve() must be true")
+		t.Error("a.Success() must be true")
 	case a.Must() != 6:
 		t.Errorf("a.Must() must return '6', got '%v'", a.Must())
 	}
 
 	switch {
 	case b.Failure() != true:
-		t.Error("b.Reject() must be true")
+		t.Error("b.Failure() must be true")
 	case b.Err() == nil:
 		t.Error("b.Err() must not be nil")
 	case b.Success():
-		t.Error("b.Resolve() must be false")
+		t.Error("b.Success() must be false")
+	}
+}
+
+func TestFlatMapPanic(t *testing.T) {
+	res := FlatMap(Success("foo"), func(x string) Try[int] {
+		// Try[T] must recover from panics
+		panic("at the discotheque")
+	})
+
+	switch {
+	case res.Failure() != true:
+		t.Error("b.Failure() must be true")
+	case res.Err() == nil:
+		t.Error("b.Err() must not be nil")
+	case res.Success():
+		t.Error("b.Success() must be false")
+	}
+
+	if res.Err().Error() != "at the discotheque" {
+		t.Errorf("got unexpected error: %s", res.Err())
 	}
 }
